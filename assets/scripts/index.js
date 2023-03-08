e3dac5b793cbb07f2db6a717082a92bc47c26547
Fatima-isab <71527458+Fatima-isab@users.noreply.github.com>
2023-03-08 17:03:59 -0600

    Add files via upload

---
+++ assets/scripts/index.js
@@ -0,0 +1,19 @@
+(function(){
+   
+    $(document).ready(function(){
+
+    $("#btnUp").click(function(){
+        $("html").animate({
+            scrollTop: 0
+        }, 'slow');
+    });
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 0){
+            $("#btnUp").fadeIn('slow');
+        }else{
+            $("#btnUp").fadeOut('slow')
+        }
+    })
+
+    });
+})();
