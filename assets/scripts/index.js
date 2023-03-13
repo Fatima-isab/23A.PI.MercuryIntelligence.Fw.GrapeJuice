20531d315b2a766b6aef0b84da9fc682a4536b88
UR13G13Z <112902642+UR13G13Z@users.noreply.github.com>
2023-03-13 11:27:01 -0600

    13/03

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,15 @@
 (function(){
-   
+    const hideBtn1 = document.getElementById("hideBtn1"),
+    hideBtn2 = document.getElementById("hideBtn2"),
+    hideBtn3 = document.getElementById("hideBtn3"),
+    hideBtn4 = document.getElementById("hideBtn4"),
+    hideBtn5 = document.getElementById("hideBtn5");
+    const alert1 = document.getElementById("alert1"),
+    alert2 = document.getElementById("alert2"),
+    alert3 = document.getElementById("alert3"),
+    alert4 = document.getElementById("alert4"),
+    alert5 = document.getElementById("alert5");
+
     $(document).ready(function(){
 
     $("#btnUp").click(function(){
@@ -16,4 +26,21 @@
     })
 
     });
+
+    hideBtn1.addEventListener("click", () => {
+        alert1.remove();
+    });
+    hideBtn2.addEventListener("click", () => {
+        alert2.remove();
+    });
+    hideBtn3.addEventListener("click", () => {
+        alert3.remove();
+    });
+    hideBtn4.addEventListener("click", () => {
+        alert4.remove();
+    });
+    hideBtn5.addEventListener("click", () => {
+        alert5.remove();
+    });
+    
 })();
