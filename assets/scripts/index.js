82aed183981665ff654f3286a28f3a61de839f79
Fatima-isab <fatima.roman2953@alumnos.udg.mx>
2023-03-13 15:38:44 -0600

    .

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,15 @@
 (function(){
-   
+    const hideBtn1 = document.getElementById("hideBtn1"),
+    hideBtn2 = document.getElementById("hideBtn2"),
+    hideBtn3 = document.getElementById("hideBtn3"),
+    hideBtn4 = document.getElementById("hideBtn4"),
+    hideBtn5 = document.getElementById("hideBtn5");
+    const alert1 = document.getElementById("alert1"),
+    alert2 = document.getElementById("alert2"),
+    alert3 = document.getElementById("alert3"),
+    alert4 = document.getElementById("alert4"),
+    alert5 = document.getElementById("alert5");
+
     $(document).ready(function(){
 
     $("#btnUp").click(function(){
